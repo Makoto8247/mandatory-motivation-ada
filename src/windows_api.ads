3756a60e95e294ae87d586071f130c3064dbdc2e
Makoto8247 <48078578+Makoto8247@users.noreply.github.com>
2025-03-03 13:31:24 +0900

    Added FindWindow and SetWindowPos to WindowsAPI.

--- src/windows_api.ads
+++ src/windows_api.ads
@@ -1,7 +1,16 @@
 with Interfaces.C; use Interfaces.C;
-with Interfaces.C.Strings;
 
 package Windows_API is
+   subtype HWND_Type is int range -2 .. 1;
+   HWND_TOPMOST : constant HWND_Type := -1;
+   HWND_NOTOPMOST : constant HWND_Type := -2;
+   HWND_DESKTOP : constant HWND_Type := 0;
+
+   SWP_NOSIZE       : constant unsigned := 1;
+   SWP_NOMOVE       : constant unsigned := 2;
+   SWP_NOZORDER     : constant unsigned := 4;
+   SWP_NOACTIVATE   : constant unsigned := 16;
+   
    -- MessageBox
    function MessageBox (
       hWnd : int;
@@ -10,4 +19,19 @@ package Windows_API is
       uType : unsigned) return int
    with Import, Convention => Stdcall, Link_Name => "MessageBoxW";
 
+   -- SetWindowPos
+   function SetWindowPos (
+      hWnd : int;
+      hWindInsertAfter : HWND_Type;
+      X : int;
+      Y : int;
+      Cx : int;
+      Cy : int;
+      uFlags : unsigned) return Boolean
+   with Import, Convention => Stdcall, Link_Name => "SetWindowPos";
+
+   function FindWindow(
+      lpClassName : access constant char16_array;
+      lpWindowName : access constant char16_array) return int
+   with Import, Convention => Stdcall, Link_Name => "FindWindowW"; 
 end Windows_API;
